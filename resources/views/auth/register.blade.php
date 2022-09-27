bae1cb9fa53703e991532fbf06eef1646692094e
Milan <milansuthar17@yahoo.com>
2022-09-28 00:27:10 +0530

    email validation updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
         <h1 class="h3 mb-3 fw-normal">Register</h1>
 
         <div class="form-group form-floating mb-3">
-            <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="name@example.com" required="required" autofocus>
+            <input type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="name@example.com"  autofocus>
             <label for="floatingEmail">Email address</label>
             @if ($errors->has('email'))
                 <span class="text-danger text-left">{{ $errors->first('email') }}</span>
@@ -17,14 +17,14 @@
         </div>
 
         <div class="form-group form-floating mb-3">
-            <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Name" required="required" autofocus>
+            <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Name" autofocus>
             <label for="floatingName">Name</label>
             @if ($errors->has('name'))
                 <span class="text-danger text-left">{{ $errors->first('name') }}</span>
             @endif
         </div>
         <div class="form-group form-floating mb-3">
-            <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
+            <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username"  autofocus>
             <label for="floatingName">Username</label>
             @if ($errors->has('username'))
                 <span class="text-danger text-left">{{ $errors->first('username') }}</span>
@@ -32,7 +32,7 @@
         </div>
 
         <div class="form-group form-floating mb-3">
-            <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
+            <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" >
             <label for="floatingPassword">Password</label>
             @if ($errors->has('password'))
                 <span class="text-danger text-left">{{ $errors->first('password') }}</span>
@@ -40,7 +40,7 @@
         </div>
 
         <div class="form-group form-floating mb-3">
-            <input type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder="Confirm Password" required="required">
+            <input type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder="Confirm Password">
             <label for="floatingConfirmPassword">Confirm Password</label>
             @if ($errors->has('password_confirmation'))
                 <span class="text-danger text-left">{{ $errors->first('password_confirmation') }}</span>
